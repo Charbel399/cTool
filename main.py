b9e7ef82682bcaec033bd15647e05bc67460a370
Charbel Sahyouni <sharbel399@gmail.com>
2024-04-11 09:46:23 -0400

    created z3_solver that takes the initial state of the diagram file and takes the z3 formula in it and outputs the solutions in json format

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import extract_uncertainty_tasks
+import z3_solver
+
+#Data Files
+xmi_file = "C:\\Users\\ashle\\OneDrive\\Desktop\\cTool\\inputs\\TorrentTO.xmi" #path xmi file
+z3_diagram_file = "C:\\Users\\ashle\OneDrive\\Desktop\\cTool\\inputs\\Initial_State.json" #path of diagram and z3 formula file
+z3_solutions_output = "C:\\Users\\ashle\\OneDrive\\Desktop\\cTool\\outputs\\solutions.json" #path of output for solutions for z3 solver
+
+
+
+
+
 
 def main():
-    tasks_names = extract_uncertainty_tasks.extract_uncertainty_tasks("C:\\Users\\ashle\\OneDrive\\Desktop\\TorrentTO.xmi")
-
-    # Print the results
-    print("The Tasks Creating Uncertainty Are:")
-    for name in tasks_names:
-             print("- " + name)
+    z3_solver.solve_formula(z3_diagram_file,z3_solutions_output)
 
 if __name__ == "__main__":
     main()
