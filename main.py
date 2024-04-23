2ff48ad5349d3079fd8d5c51dad74dd11f06e475
Charbel Sahyouni <sharbel399@gmail.com>
2024-04-23 13:13:01 -0400

    now the xmi2json outputs the belief links

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import xmi2json
 import filter
 
 #Input Files
-xmi_file = "C:\\Users\\ashle\\OneDrive\\Desktop\\cTool\\inputs\\TorrentTO.xmi" #path xmi file
+Goal_Model = "C:\\Users\\ashle\\OneDrive\\Desktop\\cTool\\inputs\\TorrentTO.xmi" #path xmi file
 z3_diagram_file = "C:\\Users\\ashle\OneDrive\\Desktop\\cTool\\inputs\\Initial_State.json" #path of diagram and z3 formula file
 may_elements = "C:\\Users\\ashle\OneDrive\\Desktop\\cTool\\inputs\\may_elements.json" #path of the file where may_elements are they can also be in the initial z3_diagram_file
 mapping_csv = "C:\\Users\\ashle\OneDrive\\Desktop\\cTool\\inputs\\goals2mayelements.csv"
@@ -18,10 +18,10 @@ uncertain_tasks_output = "C:\\Users\\ashle\\OneDrive\\Desktop\\cTool\\outputs\\u
 output_diagram_solutions = "C:\\Users\\ashle\\OneDrive\\Desktop\\cTool\\outputs\\solution_diagram" #path of the folder where solutions should be outputed
 
 def main():
-    extract_uncertainty_tasks.Uncertainty_tasks(xmi_file,uncertain_tasks_output)
+    extract_uncertainty_tasks.Uncertainty_tasks(Goal_Model,uncertain_tasks_output)
     z3_solver.solve_formula(z3_diagram_file,z3_solutions_output)
     concretization_generation.generate_concretization(z3_diagram_file,z3_solutions_output,may_elements,output_diagram_solutions)
-    xmi2json.xmi_2_json(xmi_file,xmi_2_json_output_file)
+    xmi2json.xmi_2_json(Goal_Model,xmi_2_json_output_file)
     filter.sort_solutions(z3_solutions_output,mapping_csv,output_diagram_solutions)
 
 if __name__ == "__main__":
